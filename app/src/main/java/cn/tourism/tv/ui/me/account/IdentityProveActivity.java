8db0685c869baf0dd4fa68e3b6dc747b48b183ac
caobin <12345678>
2018-06-09 11:36:45 +0800

    身份认证

--- app/src/main/java/cn/tourism/tv/ui/me/account/IdentityProveActivity.java
+++ app/src/main/java/cn/tourism/tv/ui/me/account/IdentityProveActivity.java
@@ -3,6 +3,7 @@ package cn.tourism.tv.ui.me.account;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.cb.xlibrary.statusbar.StatusBarUtils;
@@ -11,9 +12,12 @@ import cn.tourism.tv.R;
 import cn.tourism.tv.base.BaseActivity;
 
 /**
- * 账号安全
+ * 身份验证
  */
-public class AccountSafeActivity extends BaseActivity implements View.OnClickListener {
+public class IdentityProveActivity extends BaseActivity implements View.OnClickListener {
+
+    private EditText editName;
+    private EditText editIdCard;
 
 
     @Override
@@ -24,16 +28,18 @@ public class AccountSafeActivity extends BaseActivity implements View.OnClickLis
 
     @Override
     public int getRootViewId() {
-        return R.layout.activity_account_safe;
+        return R.layout.activity_identity_prove;
     }
 
     @Override
     public void initUI() {
-        ((TextView) findViewById(R.id.title)).setText("账号安全");
+        ((TextView) findViewById(R.id.title)).setText("身份认证");
         findViewById(R.id.iv_back).setOnClickListener(this);
-        findViewById(R.id.ll_psw).setOnClickListener(this);
-        findViewById(R.id.tv_phone).setOnClickListener(this);
-        findViewById(R.id.tv_real_name).setOnClickListener(this);
+        editName = (EditText) findViewById(R.id.edit_name);
+        editIdCard = (EditText) findViewById(R.id.edit_id_card);
+        findViewById(R.id.iv_front).setOnClickListener(this);
+        findViewById(R.id.iv_behind).setOnClickListener(this);
+        findViewById(R.id.btn_commit).setOnClickListener(this);
     }
 
     @Override
@@ -42,14 +48,11 @@ public class AccountSafeActivity extends BaseActivity implements View.OnClickLis
             case R.id.iv_back:
                 finish();
                 break;
-            case R.id.ll_psw://重置密码
-                launchActivity(SetPswActivity.class, null);
+            case R.id.iv_front://正面
                 break;
-            case R.id.tv_phone://手机绑定
-                launchActivity(BindPhoneActivity.class, null);
+            case R.id.iv_behind://背面
                 break;
-            case R.id.tv_real_name://实名
-                launchActivity(IdentityProveActivity.class, null);
+            case R.id.btn_commit:
                 break;
         }
     }
