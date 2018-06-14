4d197e87fc3879be6175df2c138956b2a4c6645e
caobin <12345678>
2018-06-14 14:38:21 +0600

    头像修改

--- app/src/main/java/cn/tourism/tv/ui/me/ModifyInfoActivity.java
+++ app/src/main/java/cn/tourism/tv/ui/me/ModifyInfoActivity.java
@@ -3,10 +3,13 @@ package cn.tourism.tv.ui.me;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.cb.xlibrary.dialog.XInputDialog;
+import com.cb.xlibrary.dialog.XInputDialog.InputDialogBtnClickListener;
 import com.cb.xlibrary.dialog.XUserHeadDialog;
 import com.cb.xlibrary.imagepicker.ImagePicker;
 import com.cb.xlibrary.imagepicker.bean.ImageItem;
@@ -25,8 +28,7 @@ import cn.tourism.tv.utils.GlideImagePickerLoader;
 public class ModifyInfoActivity extends BaseActivity implements View.OnClickListener {
 
     private XCircleImageView ivHead;
-    private TextView tvName;
-
+    private TextView tvName, tvAge, tvAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +47,15 @@ public class ModifyInfoActivity extends BaseActivity implements View.OnClickList
 
         ivHead = (XCircleImageView) findViewById(R.id.iv_head);
         tvName = (TextView) findViewById(R.id.tv_name);
+        tvAge = (TextView) findViewById(R.id.tv_age);
+        tvAddress = (TextView) findViewById(R.id.tv_address);
 
         findViewById(R.id.iv_back).setOnClickListener(this);
         findViewById(R.id.ll_age).setOnClickListener(this);
         findViewById(R.id.ll_address).setOnClickListener(this);
         findViewById(R.id.btn_save).setOnClickListener(this);
         ivHead.setOnClickListener(this);
+        tvName.setOnClickListener(this);
     }
 
     @Override
@@ -60,10 +65,14 @@ public class ModifyInfoActivity extends BaseActivity implements View.OnClickList
                 finish();
                 break;
             case R.id.ll_age:
+                showInputDialog("请输入年龄", InputType.TYPE_CLASS_NUMBER);
                 break;
             case R.id.ll_address:
                 break;
             case R.id.btn_save:
+                break;
+            case R.id.tv_name:
+                showInputDialog("请输入昵称", InputType.TYPE_CLASS_TEXT);
                 break;
             case R.id.iv_head:
                 XUserHeadDialog xUserHeadDialog = new XUserHeadDialog(ModifyInfoActivity.this);
@@ -73,6 +82,32 @@ public class ModifyInfoActivity extends BaseActivity implements View.OnClickList
         }
     }
 
+    /**
+     * 弹出输入框
+     *
+     * @param title
+     */
+    private void showInputDialog(final String title, int type) {
+        new XInputDialog
+                .Builder(ModifyInfoActivity.this)
+                .title(title)
+                .leftBtnTxt("取消")
+                .rightBtnTxt("确定")
+                .setCancelable(false)
+                .setEditInputType(type)
+                .rightBtnTxtColor(Color.parseColor("#01999D"))
+                .setSureClickListener(new InputDialogBtnClickListener() {
+                    @Override
+                    public void onClick(String content) {
+                        if (title.equals("请输入昵称")) {
+                            tvName.setText(content);
+                        } else {
+                            tvAge.setText(content + "");
+                        }
+                    }
+                })
+                .create().show();
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
